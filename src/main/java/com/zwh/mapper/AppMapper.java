313297e05ca99d789f4d66eef82ae9af25dd0989
张蘶豪 <1840816813@qq.com>
2025-08-04 11:20:00 +0800

    feat: 实现应用管理服务层
    - 创建应用 CRUD 基础操作
    - 实现应用审核和发布流程
    - 添加应用分页查询和筛选
    - 支持应用状态管理

---
+++ src/main/java/com/zwh/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.zwh.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.zwh.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/zwh-W">张蘶豪</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
